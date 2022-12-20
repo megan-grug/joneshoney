ae1452362cdaaf0b85d6caca8fd7aa353f2bc812
megan-grug <meganjones.cymru@gmail.com>
2022-12-20 19:25:03 +0000

    added bag app and page

--- joneshoneyproject/settings.py
+++ joneshoneyproject/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -74,7 +75,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # required by allauth
+                'django.template.context_processors.request',  # need4allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
